78d457307716ffcd792d55898fc4490efe97a1e9
Sayman7 <buisnesssayman@gmail.com>
2025-02-22 14:32:06 -0500

    portfolio setup1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")  # Ensure templates folder is set
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Runs on http://127.0.0.1:5000
+    app.run()
