a57928c440369dd38c1b8941484d96852d57240c
Sayman7 <buisnesssayman@gmail.com>
2025-02-22 16:38:34 -0500

    Replaced mysqlclient with pymysql for Vercel

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from flask import Flask, render_template
-import os
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-def handler(event, context):
-    from werkzeug.middleware.proxy_fix import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-    return app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))  # Vercel uses 3000
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
