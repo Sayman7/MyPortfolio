65791631821d88b8aa713019865881c87eeb08e9
Sayman7 <buisnesssayman@gmail.com>
2025-05-23 10:32:51 -0400

    fixed deployement errors #2

--- src/components/MacbookScroll.tsx
+++ src/components/MacbookScroll.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 import { motion, useScroll, useTransform, MotionValue } from "framer-motion";
 import DellLogo from "@/components/DellLogo";
 import { cn } from "@/lib/utils";
@@ -149,7 +150,7 @@ export const Lid = ({
           className="absolute inset-0 flex items-center justify-center rounded-lg bg-[#010101]"
         >
           <span className="text-white">
-            <DellLogo/>
+            <DellLogo />
           </span>
         </div>
       </div>
@@ -165,12 +166,12 @@ export const Lid = ({
         className="absolute inset-0 h-98 w-[32rem] rounded-2xl bg-[#010101] p-2"
       >
         <div className="absolute inset-0 rounded-lg bg-[#272729]" />
-        <img
-         src="/Sayman_desc.png"
-         alt="Sayman's description"
-         className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
-         loading="lazy"
-         />
+        <Image
+          src="/Sayman_desc.png"
+          alt="Sayman's description"
+          fill
+          className="rounded-lg object-cover object-left-top"
+        />
       </motion.div>
     </div>
   );
@@ -634,23 +635,3 @@ export const OptionKey = ({ className }: { className: string }) => {
   );
 };
 
-const AceternityLogo = () => {
-  return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-3 w-3 text-white"
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-};
