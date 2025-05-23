a33e7a75ff832f170621b9b7566ff53688f786d8
Sayman7 <buisnesssayman@gmail.com>
2025-05-23 13:34:44 -0400

    seo trial #8

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://sayman-lal.vercel.app',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://sayman-lal.vercel.app/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
